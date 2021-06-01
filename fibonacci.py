deb0f9e6b276a4682ea72152f232d31457771c51
Nastassia Kamiakova <nastassia294@gmail.com>
2021-06-01 13:01:11 +0300

    remove print()

--- fibonacci.py
+++ fibonacci.py
@@ -35,13 +35,6 @@ def head(it, count):
         count -= 1
 
 
-print("head of an empty list:", list(head([], 10)))
-print("head of a  short list:", list(head([1, 2, 3], 10)))
-print("empty head of  a list:", list(head([1, 2, 3], 0)))
-print("head(Fibonacci) result:", list(head(Fibonacci(), 10)))
-print("head(fibonacci) result:", list(head(fibonacci(), 10)))
-
-
 # Skips the first `count` elements from the given iterable and returns all the remaining elements.
 def skip(it, count):
     while True:
@@ -53,12 +46,6 @@ def skip(it, count):
             count -= 1
         else:
             yield a
-
-
-print("skip(head(...), ...) function result")
-print(list(skip(head(fibonacci(), 10), 10)))
-print("head(skip(...), ...) function result")
-print(list(head(skip(fibonacci(), 10), 10)))
 
 
 # From the given iterable returns elements in even positions.
@@ -73,10 +60,6 @@ def even(it):
             break
 
 
-print("even function result")
-print(list(even(["a", "b", "c", "d", "e", "f"])))
-
-
 # From the given iterable returns elements in odd positions.
 # [a, b, c, d, e, f] -> [b, d, f]
 def odd(it):
@@ -87,22 +70,3 @@ def odd(it):
             yield next(it)
         except StopIteration:
             break
-
-
-print("odd function result")
-print(list(odd(["a", "b", "c", "d", "e", "f"])))
-
-print("--- first 10 Fibonacci numbers")
-print(list(head(fibonacci(), 10)))
-
-print("--- first 10 Fibonacci numbers that are in the even positions")
-print(list(head(even(fibonacci()), 10)))
-
-print("--- first 10 Fibonacci numbers ...")
-print(list(even(head(fibonacci(), 10))))
-
-print("--- first 10 Fibonacci numbers that are in the odd positions")
-print(list(head(odd(fibonacci()), 10)))
-
-print("--- first 10 Fibonacci numbers ...")
-print(list(odd(head(fibonacci(), 10))))
