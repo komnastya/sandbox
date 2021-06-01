74eab102f7278c22e4acef92cd8952bf94962f5f
Nastassia Kamiakova <nastassia294@gmail.com>
2021-06-01 12:56:13 +0300

    merge task

---
+++ merge.py
@@ -0,0 +1,15 @@
+# Merges two sorted sequences of numbers into a single sorted sequence.
+#
+# merge([], []) -> []
+# merge([1, 2, 3], []) -> [1, 2, 3]
+# merge([], [1, 2, 3]) -> [1, 2, 3]
+# merge([1], [2]) -> [1, 2]
+# merge([2], [1]) -> [1, 2]
+# merge([1, 3, 5], [2, 4, 6]) -> [1, 2, 3, 4, 5, 6]
+# merge([2, 4, 6], [1, 3, 5]) -> [1, 2, 3, 4, 5, 6]
+# merge([1, 1, 1], [2, 2, 2]) -> [1, 1, 1, 2, 2, 2]
+#
+# Both `a` and `b` are iterables, and the result must be iterable too. Don't use lists! Use `yield`.
+# Both `a` and `b` are sorted, and the result must be sorted too.
+def merge(a, b):
+    pass
