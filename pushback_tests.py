a194860d5332c62204bf5453fa3bf4d0a115bfa8
Nastassia Kamiakova <nastassia294@gmail.com>
2021-06-03 14:38:04 +0300

    No numbers, only nums!

--- pushback_tests.py
+++ pushback_tests.py
@@ -4,24 +4,24 @@ from pushback import PushBackIterator
 
 class TestMerge(unittest.TestCase):
     def test_push_back_iterator(self):
-        numbers = PushBackIterator(iter([1, 2, 3, 4, 5]))
-        self.assertEqual(next(numbers), 1)
-        self.assertEqual(next(numbers), 2)
-        self.assertEqual(next(numbers), 3)
-        self.assertEqual(next(numbers), 4)
-        self.assertEqual(next(numbers), 5)
+        nums = PushBackIterator(iter([1, 2, 3, 4, 5]))
+        self.assertEqual(next(nums), 1)
+        self.assertEqual(next(nums), 2)
+        self.assertEqual(next(nums), 3)
+        self.assertEqual(next(nums), 4)
+        self.assertEqual(next(nums), 5)
 
     def test_push_back_method(self):
-        numbers = PushBackIterator(iter([3, 4, 5]))
-        self.assertEqual(next(numbers), 3)
+        nums = PushBackIterator(iter([3, 4, 5]))
+        self.assertEqual(next(nums), 3)
 
-        numbers.push_back(3)
-        self.assertEqual(next(numbers), 3)
+        nums.push_back(3)
+        self.assertEqual(next(nums), 3)
 
-        numbers.push_back(3)
-        numbers.push_back(2)
-        numbers.push_back(1)
-        self.assertEqual(list(numbers), [1, 2, 3, 4, 5])
+        nums.push_back(3)
+        nums.push_back(2)
+        nums.push_back(1)
+        self.assertEqual(list(nums), [1, 2, 3, 4, 5])
 
     def test_has_next_method(self):
         nums = PushBackIterator(iter([]))
