56a30ba91d171871210bbb90831b6d4f5abe70f8
Nastassia Kamiakova <nastassia294@gmail.com>
2021-06-03 14:59:59 +0300

    add tests for PushBackIterator

--- pushback_tests.py
+++ pushback_tests.py
@@ -5,11 +5,7 @@ from pushback import PushBackIterator
 class TestMerge(unittest.TestCase):
     def test_push_back_iterator(self):
         nums = PushBackIterator(iter([1, 2, 3, 4, 5]))
-        self.assertEqual(next(nums), 1)
-        self.assertEqual(next(nums), 2)
-        self.assertEqual(next(nums), 3)
-        self.assertEqual(next(nums), 4)
-        self.assertEqual(next(nums), 5)
+        self.assertEqual(list(nums), [1, 2, 3, 4, 5])
 
     def test_push_back_method(self):
         nums = PushBackIterator(iter([3, 4, 5]))
@@ -51,6 +47,11 @@ class TestMerge(unittest.TestCase):
 
         self.assertEqual(next(nums), 5)
         self.assertFalse(nums.has_next())
+
+        nums = PushBackIterator(iter([]))
+        self.assertFalse(nums.has_next())
+        nums.push_back(1)
+        self.assertEqual(list(nums), [1])
 
 
 if __name__ == "__main__":
