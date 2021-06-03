2790e835abb61d26c9447cea15f90c7ae6ac0cbc
Nastassia Kamiakova <nastassia294@gmail.com>
2021-06-03 14:14:54 +0300

    add tests for has_next method

--- pushback_tests.py
+++ pushback_tests.py
@@ -23,6 +23,35 @@ class TestMerge(unittest.TestCase):
         numbers.push_back(1)
         self.assertEqual(list(numbers), [1, 2, 3, 4, 5])
 
+    def test_has_next_method(self):
+        nums = PushBackIterator(iter([]))
+        self.assertFalse(nums.has_next())
+
+        nums = PushBackIterator(iter([1, 2, 3]))
+        self.assertEqual(next(nums), 1)
+
+        self.assertTrue(nums.has_next())
+        self.assertEqual(next(nums), 2)
+
+        self.assertTrue(nums.has_next())
+        self.assertEqual(next(nums), 3)
+
+        self.assertFalse(nums.has_next())
+
+    def test_has_next_and_push_back_methods_together(self):
+        nums = PushBackIterator(iter([3, 4]))
+
+        self.assertEqual(next(nums), 3)
+        self.assertEqual(next(nums), 4)
+
+        self.assertFalse(nums.has_next())
+
+        nums.push_back(5)
+        self.assertTrue(nums.has_next())
+
+        self.assertEqual(next(nums), 5)
+        self.assertFalse(nums.has_next())
+
 
 if __name__ == "__main__":
     unittest.main()
