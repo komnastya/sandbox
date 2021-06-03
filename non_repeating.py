39bd35a486801ccd69cdc5573eaf2be8dfa14861
Nastassia Kamiakova <nastassia294@gmail.com>
2021-06-03 19:37:41 +0300

    non_repeating can be better

--- non_repeating.py
+++ non_repeating.py
@@ -6,8 +6,17 @@
 # If `it` is sorted, then obviously this function generates only unique elements.
 # If `it` is not sorted, then the generated elements are not unique in the whole sequence,
 # but there are no equal consecutive elements.
-def non_repeating(it):
-    """Yields elements from it, but only thise which are not repeating."""
+def non_repeating_one(it):
+    it = iter(it)
+    x = next(it, None)
+    while x is not None:
+        y = x
+        x = next(it, None)
+        if x != y:
+            yield y
+
+
+def non_repeating_two(it):
     it = iter(it)
     x = next(it, None)
     y = next(it, None)
@@ -17,4 +26,19 @@ def non_repeating(it):
         x = y
         y = next(it, None)
     if x is not None:
-      yield x
+        yield x
+
+
+def non_repeating_better(it):
+    it = iter(it)
+    last = None
+    has_last = False
+    while True:
+        try:
+            current = next(it)
+        except StopIteration:
+            break
+        if not has_last or current != last:
+            yield current
+        last = current
+        has_last = True
--- non_repeating_tests.py
+++ non_repeating_tests.py
@@ -1,17 +1,41 @@
 import unittest
-from non_repeating import non_repeating
+from non_repeating import non_repeating_one, non_repeating_two, non_repeating_better
 
 
 class TestNonRepeating(unittest.TestCase):
-    def test_non_repeating(self):
-        self.assertEqual(list(non_repeating([])), [])
-        self.assertEqual(list(non_repeating([1, 2, 3, 4, 5])), [1, 2, 3, 4, 5])
-        self.assertEqual(list(non_repeating([1, 2, 1, 2, 1])), [1, 2, 1, 2, 1])
-        self.assertEqual(list(non_repeating([1, 1, 1, 1, 1])), [1])
-        self.assertEqual(list(non_repeating([1, 1, 2, 2, 2])), [1, 2])
+    def test_non_repeating_one(self):
+        self.assertEqual(list(non_repeating_one([])), [])
+        self.assertEqual(list(non_repeating_one([1, 2, 3, 4, 5])), [1, 2, 3, 4, 5])
+        self.assertEqual(list(non_repeating_one([1, 2, 1, 2, 1])), [1, 2, 1, 2, 1])
+        self.assertEqual(list(non_repeating_one([1, 1, 1, 1, 1])), [1])
+        self.assertEqual(list(non_repeating_one([1, 1, 2, 2, 2])), [1, 2])
         self.assertEqual(
-            list(non_repeating([1, 2, 2, 3, 4, 4, 5, 6, 6])), [1, 2, 3, 4, 5, 6]
+            list(non_repeating_one([1, 2, 2, 3, 4, 4, 5, 6, 6])), [1, 2, 3, 4, 5, 6]
         )
+
+    def test_non_repeating_two(self):
+        self.assertEqual(list(non_repeating_two([])), [])
+        self.assertEqual(list(non_repeating_two([1, 2, 3, 4, 5])), [1, 2, 3, 4, 5])
+        self.assertEqual(list(non_repeating_two([1, 2, 1, 2, 1])), [1, 2, 1, 2, 1])
+        self.assertEqual(list(non_repeating_two([1, 1, 1, 1, 1])), [1])
+        self.assertEqual(list(non_repeating_two([1, 1, 2, 2, 2])), [1, 2])
+        self.assertEqual(
+            list(non_repeating_two([1, 2, 2, 3, 4, 4, 5, 6, 6])), [1, 2, 3, 4, 5, 6]
+        )
+
+    def test_non_repeating_better(self):
+        self.assertEqual(list(non_repeating_better([])), [])
+        self.assertEqual(list(non_repeating_better([None, None, None])),
+                         [None])
+        self.assertEqual(list(non_repeating_better([1, 2, 3, 4, 5])),
+                         [1, 2, 3, 4, 5])
+        self.assertEqual(list(non_repeating_better([1, 2, 1, 2, 1])),
+                         [1, 2, 1, 2, 1])
+        self.assertEqual(list(non_repeating_better([1, 1, 1, 1, 1])), [1])
+        self.assertEqual(list(non_repeating_better([1, 1, 2, 2, 2])), [1, 2])
+        self.assertEqual(list(non_repeating_better([1, 2, 2, 3, 4, 4, 5, 6, 6])),
+                         [1, 2, 3, 4, 5, 6])
+
 
 
 if __name__ == "__main__":
