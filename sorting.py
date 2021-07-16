466092705740ee6d52186a53c5926b41408e20f8
Nastassia Kamiakova <nastassia294@gmail.com>
2021-07-16 14:00:34 +0300

    Rename functions

--- sorting.py
+++ sorting.py
@@ -1,4 +1,4 @@
-def sorting(list):
+def bubble_sort(list):
     for j in range(len(list) - 1):
         for i in range(len(list) - 1):
             if list[i] > list[i + 1]:
@@ -8,7 +8,7 @@ def sorting(list):
     return list
 
 
-def sorting2(list):
+def select_sort(list):
     for j in range(len(list)):
         for i in range(j + 1, len(list)):
             if list[i] < list[j]:
--- test_sorting.py
+++ test_sorting.py
@@ -1,16 +1,18 @@
 import pytest
-from sorting import sorting, sorting2
+from sorting import bubble_sort, select_sort
+
 
 def test_sorting():
-  assert sorting([]) == []
-  assert sorting([1]) == [1]
-  assert sorting([1,2]) == [1,2]
-  assert sorting([2,1]) == [1,2]
-  assert sorting([5,3,1,4,2]) == [1,2,3,4,5]
+    assert bubble_sort([]) == []
+    assert bubble_sort([1]) == [1]
+    assert bubble_sort([1, 2]) == [1, 2]
+    assert bubble_sort([2, 1]) == [1, 2]
+    assert bubble_sort([5, 3, 1, 4, 2]) == [1, 2, 3, 4, 5]
+
 
 def test_sorting2():
-  assert sorting2([]) == []
-  assert sorting2([1]) == [1]
-  assert sorting2([1,2]) == [1,2]
-  assert sorting2([2,1]) == [1,2]
-  assert sorting2([5,3,1,4,2]) == [1,2,3,4,5]
+    assert select_sort([]) == []
+    assert select_sort([1]) == [1]
+    assert select_sort([1, 2]) == [1, 2]
+    assert select_sort([2, 1]) == [1, 2]
+    assert select_sort([5, 3, 1, 4, 2]) == [1, 2, 3, 4, 5]
