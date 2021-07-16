06c22a0aa0e8479d42d4b34d624a96257c78d432
Nastassia Kamiakova <nastassia294@gmail.com>
2021-07-16 17:31:03 +0300

    Improve insert_sort

--- sorting.py
+++ sorting.py
@@ -22,5 +22,5 @@ def insert_sort(list):
     for i in range(1, len(list)):
         for j in range(0, i):
             if list[i] < list[j]:
-                list[j:j] = [list.pop(i)]
+                list.insert(j, list.pop(i))
     return list
