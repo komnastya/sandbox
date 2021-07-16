a9d9df228d5165884375b69bd1d2aede55f8b044
Nastassia Kamiakova <nastassia294@gmail.com>
2021-07-16 17:53:01 +0300

    Improve unit tests for sort functions

--- test_sorting.py
+++ test_sorting.py
@@ -1,26 +1,26 @@
 import pytest
-from sorting import bubble_sort, select_sort, insert_sort
+from sorting import bubble_sort, select_sort, insert_sort, quick_sort
+
+
+def _check_sorting_function(sf):
+    assert sf([]) == []
+    assert sf([1]) == [1]
+    assert sf([1, 2]) == [1, 2]
+    assert sf([2, 1]) == [1, 2]
+    assert sf([5, 3, 1, 4, 2]) == [1, 2, 3, 4, 5]
 
 
 def test_bubble_sort():
-    assert bubble_sort([]) == []
-    assert bubble_sort([1]) == [1]
-    assert bubble_sort([1, 2]) == [1, 2]
-    assert bubble_sort([2, 1]) == [1, 2]
-    assert bubble_sort([5, 3, 1, 4, 2]) == [1, 2, 3, 4, 5]
+    _check_sorting_function(bubble_sort)
 
 
 def test_select_sort():
-    assert select_sort([]) == []
-    assert select_sort([1]) == [1]
-    assert select_sort([1, 2]) == [1, 2]
-    assert select_sort([2, 1]) == [1, 2]
-    assert select_sort([5, 3, 1, 4, 2]) == [1, 2, 3, 4, 5]
+    _check_sorting_function(select_sort)
 
 
 def test_insert_sort():
-    assert insert_sort([]) == []
-    assert insert_sort([1]) == [1]
-    assert insert_sort([1, 2]) == [1, 2]
-    assert insert_sort([2, 1]) == [1, 2]
-    assert insert_sort([5, 3, 1, 4, 2]) == [1, 2, 3, 4, 5]
+    _check_sorting_function(insert_sort)
+
+
+def test_quick_sort():
+    _check_sorting_function(quick_sort)
