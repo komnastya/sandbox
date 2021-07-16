4307405c1e99513fc153bd8d0f32f032c1277271
Nastassia Kamiakova <nastassia294@gmail.com>
2021-07-16 14:14:43 +0300

    Rename test functions

--- test_sorting.py
+++ test_sorting.py
@@ -2,7 +2,7 @@ import pytest
 from sorting import bubble_sort, select_sort
 
 
-def test_sorting():
+def test_bubble_sort():
     assert bubble_sort([]) == []
     assert bubble_sort([1]) == [1]
     assert bubble_sort([1, 2]) == [1, 2]
@@ -10,7 +10,7 @@ def test_sorting():
     assert bubble_sort([5, 3, 1, 4, 2]) == [1, 2, 3, 4, 5]
 
 
-def test_sorting2():
+def test_select_sort():
     assert select_sort([]) == []
     assert select_sort([1]) == [1]
     assert select_sort([1, 2]) == [1, 2]
