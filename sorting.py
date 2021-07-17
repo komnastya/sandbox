b4638ef64e8774d4e9519983ab1f618c083a5790
Nastassia Kamiakova <nastassia294@gmail.com>
2021-07-17 13:08:54 +0300

    Impove bubble sort

--- sorting.py
+++ sorting.py
@@ -1,10 +1,8 @@
 def bubble_sort(list):
-    for j in range(len(list) - 1):
+    for j in range(len(list)):
         for i in range(len(list) - 1):
             if list[i] > list[i + 1]:
-                current = list[i]
-                list[i] = list[i + 1]
-                list[i + 1] = current
+                list[i], list[i + 1] = list[i + 1], list[i]
     return list
 
 
