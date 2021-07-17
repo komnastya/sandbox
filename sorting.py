14d77939aa06cf0d507c21422be050275cf41dda
Nastassia Kamiakova <nastassia294@gmail.com>
2021-07-17 12:17:39 +0300

    Add quick sort

--- sorting.py
+++ sorting.py
@@ -24,3 +24,27 @@ def insert_sort(list):
             if list[i] < list[j]:
                 list.insert(j, list.pop(i))
     return list
+
+
+def quick_sort(arr):
+    def portition(arr, l, r):
+        pivot = arr[r]
+        i = l - 1
+        for j in range(l, r):
+            if arr[j] < pivot:
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[i + 1], arr[r] = arr[r], arr[i + 1]
+        return i + 1
+
+    def qs(arr, l, r):
+        if l >= r:
+            return list
+        p = portition(arr, l, r)
+
+        qs(arr, l, p - 1)
+        qs(arr, p + 1, r)
+
+    qs(arr, 0, len(arr) - 1)
+
+    return arr
