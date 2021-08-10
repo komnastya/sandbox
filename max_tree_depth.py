c72204894c9936d46a643675b6df385365df17a3
Nastassia Kamiakova <nastassia294@gmail.com>
2021-08-10 18:08:59 +0300

    Add max_tree_depth function with unit test

---
+++ max_tree_depth.py
@@ -0,0 +1,36 @@
+# Find tree height
+
+#                        Node
+#       left              |               right
+#         ----------------+------------------
+#         |                                 |
+#    x----+-----                       -----+-----
+#              |                       |         |
+#          x---+---x                ---+---x x---+---x
+#                                   |
+#                               x---+---x
+
+
+class Node:
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+
+# Count quantity
+
+
+def node_count(node):
+    if node is None:
+        return 0
+    else:
+        return 1 + node_count(node.left) + node_count(node.right)
+
+
+# Count height
+
+
+def max_tree_depth(node):
+    if node is None:
+        return 0
+    return 1 + max(max_tree_depth(node.left), max_tree_depth(node.right))
---
+++ max_tree_depth_test.py
@@ -0,0 +1,9 @@
+import pytest
+from max_tree_depth import max_tree_depth, Node
+
+
+def test_max_tree_depth():
+    assert max_tree_depth(None) == 0
+    assert max_tree_depth(Node()) == 1
+    assert max_tree_depth(Node(left=Node(), right=Node())) == 2
+    assert max_tree_depth(Node(left=None, right=Node(left=None, right=Node()))) == 3
