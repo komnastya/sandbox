378eeee3874f3f2269d798ba979c6218ed847f38
Nastassia Kamiakova <nastassia294@gmail.com>
2021-09-03 21:18:36 +0300

    Maximum Depth of N-ary Tree with unit test

---
+++ depth_of_n_ary_tree.py
@@ -0,0 +1,16 @@
+# Given a n-ary tree, find its maximum depth.
+# The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+# Definition for a Node.
+
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+def maxDepth(node):
+    if node is None:
+        return 0
+    if not node.children:
+        return 1
+    return 1 + max((maxDepth(child) for child in node.children))
---
+++ depth_of_n_ary_tree_test.py
@@ -0,0 +1,13 @@
+from depth_of_n_ary_tree import maxDepth, Node
+
+
+def test_max_depth():
+        assert maxDepth(None) == 0
+        assert maxDepth(Node()) == 1
+        assert maxDepth(Node(val=None, children=[Node()])) == 2
+        assert maxDepth(Node(val=None, children=[Node(val=None, children=[Node()])])) == 3
+        assert maxDepth(Node(val=None, children=[Node(val=None, children=[Node(val=None, children=[Node()])])])) == 4
+        assert maxDepth(Node(val=None, children=[
+                Node(val=None, children=[Node(val=None, children=[Node(val=None, children=[Node()])])])])) == 5
+        assert maxDepth(Node(val=None, children=[Node(val=None, children=[
+                Node(val=None, children=[Node(val=None, children=[Node(val=None, children=[Node()])])])])])) == 6
