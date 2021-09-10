4a0eac292dfe2ee30d743f096b82b53dbc3c768a
Nastassia Kamiakova <nastassia294@gmail.com>
2021-09-10 12:48:44 +0300

    The find_numbers_2

--- findnumbers.py
+++ findnumbers.py
@@ -24,3 +24,21 @@ def find_numbers(s: str) -> List[int]:
             i += 1
             continue
     return output
+
+
+def find_numbers_2(s: str) -> List[int]:
+    output = []
+    i = 0
+    while i < len(s):
+        char = ord(s[i])
+        if char >= 48 and char <= 57:
+            j = i + 1
+            while j < len(s):
+                char = ord(s[j])
+                if not (char >= 48 and char <= 57):
+                    break
+                j += 1
+            output.append(int(s[i:j]))
+            i = j
+        i += 1
+    return output
