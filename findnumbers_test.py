d6a3dece51956b924c766d6ecc44b9c6175bdbeb
Nastassia Kamiakova <nastassia294@gmail.com>
2021-09-10 18:56:02 +0300

    Add the tests for the find_number_sm function

--- findnumbers_test.py
+++ findnumbers_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from findnumbers import find_numbers, find_numbers_2
+from findnumbers import find_numbers, find_numbers_2, find_numbers_sm
 
 
 def test_findnumbers():
@@ -43,4 +43,33 @@ def test_find_numbers2():
     assert list(nums) == [2, 34, 56, 78]
 
     nums = find_numbers_2('-1+2')
+    assert list(nums) == [1, 2]
+
+
+def test_find_numbers_sm():
+    nums = find_numbers_sm('')
+    assert list(nums) == []
+
+    nums = find_numbers_sm('no numbers here')
+    assert list(nums) == []
+
+    nums = find_numbers_sm('0')
+    assert list(nums) == [0]
+
+    nums = find_numbers_sm('012')
+    assert list(nums) == [12]
+
+    nums = find_numbers_sm('0 1 2')
+    assert list(nums) == [0, 1, 2]
+
+    nums = find_numbers_sm('0, 1, 2,')
+    assert list(nums) == [0, 1, 2]
+
+    nums = find_numbers_sm('(123)')
+    assert list(nums) == [123]
+
+    nums = find_numbers_sm('2some34numbers56here78"')
+    assert list(nums) == [2, 34, 56, 78]
+
+    nums = find_numbers_sm('-1+2')
     assert list(nums) == [1, 2]
