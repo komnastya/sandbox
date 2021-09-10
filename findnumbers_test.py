1831d8ed3bfaae0d842cd8c11175b03a6ec49b72
Nastassia Kamiakova <nastassia294@gmail.com>
2021-09-10 16:13:09 +0300

    Add the tests for the 'find_numbers_2' function

--- findnumbers_test.py
+++ findnumbers_test.py
@@ -1,4 +1,6 @@
-from findnumbers import find_numbers
+import pytest
+
+from findnumbers import find_numbers, find_numbers_2
 
 
 def test_findnumbers():
@@ -11,3 +13,42 @@ def test_findnumbers():
     assert find_numbers("(123)") == [123]
     assert find_numbers("12some34numbers56here78") == [12, 34, 56, 78]
     assert find_numbers("-1+2") == [1, 2]
+
+
+def test_find_numbers2():
+    nums = find_numbers_2('')
+    with pytest.raises(StopIteration):
+        next(nums)
+
+    nums = find_numbers_2('no numbers here')
+    with pytest.raises(StopIteration):
+        next(nums)
+
+    nums = find_numbers_2('0')
+    assert next(nums) == 0
+
+    nums = find_numbers_2('012')
+    assert next(nums) == 12
+
+    nums = find_numbers_2('0 1 2')
+    assert next(nums) == 0
+    assert next(nums) == 1
+    assert next(nums) == 2
+
+    nums = find_numbers_2('0, 1, 2,')
+    assert next(nums) == 0
+    assert next(nums) == 1
+    assert next(nums) == 2
+
+    nums = find_numbers_2('(123)')
+    assert next(nums) == 123
+
+    nums = find_numbers_2('2some34numbers56here78"')
+    assert next(nums) == 2
+    assert next(nums) == 34
+    assert next(nums) == 56
+    assert next(nums) == 78
+
+    nums = find_numbers_2('-1+2')
+    assert next(nums) == 1
+    assert next(nums) == 2
