a853d14722b0fd94f03981329c2c273ad74ab6f0
Nastassia Kamiakova <nastassia294@gmail.com>
2021-09-11 20:27:50 +0300

    Add the test for the reformat_numbers_sm_bool

--- findnumbers_test.py
+++ findnumbers_test.py
@@ -2,6 +2,7 @@ from findnumbers import (
     find_numbers,
     find_numbers_2,
     find_numbers_sm,
+    find_numbers_sm_bool,
 )
 
 
@@ -43,6 +44,35 @@ def test_find_numbers2():
     assert list(nums) == [2, 34, 56, 78]
 
     nums = find_numbers_2("-1+2")
+    assert list(nums) == [1, 2]
+
+
+def test_find_numbers_sm_bool():
+    nums = find_numbers_sm_bool("")
+    assert list(nums) == []
+
+    nums = find_numbers_sm_bool("no numbers here")
+    assert list(nums) == []
+
+    nums = find_numbers_sm_bool("0")
+    assert list(nums) == [0]
+
+    nums = find_numbers_sm_bool("012")
+    assert list(nums) == [12]
+
+    nums = find_numbers_sm_bool("0 1 2")
+    assert list(nums) == [0, 1, 2]
+
+    nums = find_numbers_sm_bool("0, 1, 2,")
+    assert list(nums) == [0, 1, 2]
+
+    nums = find_numbers_sm_bool("(123)")
+    assert list(nums) == [123]
+
+    nums = find_numbers_sm_bool('2some34numbers56here78"')
+    assert list(nums) == [2, 34, 56, 78]
+
+    nums = find_numbers_sm_bool("-1+2")
     assert list(nums) == [1, 2]
 
 
