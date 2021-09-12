c5e109d8c696aa048ea0eda59c01d23b10365cf4
Nastassia Kamiakova <nastassia294@gmail.com>
2021-09-12 16:09:15 +0300

    Fix few stylistic issues

--- findnumbers_test.py
+++ findnumbers_test.py
@@ -19,87 +19,41 @@ def test_findnumbers():
 
 
 def test_find_numbers2():
-    nums = find_numbers_2("")
-    assert list(nums) == []
-
-    nums = find_numbers_2("no numbers here")
-    assert list(nums) == []
-
-    nums = find_numbers_2("0")
-    assert list(nums) == [0]
-
-    nums = find_numbers_2("012")
-    assert list(nums) == [12]
-
-    nums = find_numbers_2("0 1 2")
-    assert list(nums) == [0, 1, 2]
-
-    nums = find_numbers_2("0, 1, 2,")
-    assert list(nums) == [0, 1, 2]
-
-    nums = find_numbers_2("(123)")
-    assert list(nums) == [123]
-
-    nums = find_numbers_2('2some34numbers56here78"')
-    assert list(nums) == [2, 34, 56, 78]
-
-    nums = find_numbers_2("-1+2")
-    assert list(nums) == [1, 2]
+    run_test_cases(find_numbers_2)
 
 
 def test_find_numbers_sm_bool():
-    nums = find_numbers_sm_bool("")
-    assert list(nums) == []
-
-    nums = find_numbers_sm_bool("no numbers here")
-    assert list(nums) == []
-
-    nums = find_numbers_sm_bool("0")
-    assert list(nums) == [0]
-
-    nums = find_numbers_sm_bool("012")
-    assert list(nums) == [12]
-
-    nums = find_numbers_sm_bool("0 1 2")
-    assert list(nums) == [0, 1, 2]
-
-    nums = find_numbers_sm_bool("0, 1, 2,")
-    assert list(nums) == [0, 1, 2]
-
-    nums = find_numbers_sm_bool("(123)")
-    assert list(nums) == [123]
-
-    nums = find_numbers_sm_bool('2some34numbers56here78"')
-    assert list(nums) == [2, 34, 56, 78]
-
-    nums = find_numbers_sm_bool("-1+2")
-    assert list(nums) == [1, 2]
+    run_test_cases(find_numbers_sm_bool)
 
 
 def test_find_numbers_sm():
-    nums = find_numbers_sm("")
+    run_test_cases(find_numbers_sm)
+
+
+def run_test_cases(func):
+    nums = func("")
     assert list(nums) == []
 
-    nums = find_numbers_sm("no numbers here")
+    nums = func("no numbers here")
     assert list(nums) == []
 
-    nums = find_numbers_sm("0")
+    nums = func("0")
     assert list(nums) == [0]
 
-    nums = find_numbers_sm("012")
+    nums = func("012")
     assert list(nums) == [12]
 
-    nums = find_numbers_sm("0 1 2")
+    nums = func("0 1 2")
     assert list(nums) == [0, 1, 2]
 
-    nums = find_numbers_sm("0, 1, 2,")
+    nums = func("0, 1, 2,")
     assert list(nums) == [0, 1, 2]
 
-    nums = find_numbers_sm("(123)")
+    nums = func("(123)")
     assert list(nums) == [123]
 
-    nums = find_numbers_sm('2some34numbers56here78"')
+    nums = func('2some34numbers56here78"')
     assert list(nums) == [2, 34, 56, 78]
 
-    nums = find_numbers_sm("-1+2")
+    nums = func("-1+2")
     assert list(nums) == [1, 2]
