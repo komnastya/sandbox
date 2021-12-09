7916347eac8e249ac58354e2585639afe8d5a82b
Nastassia Kamiakova <nastassia294@gmail.com>
2021-12-09 14:03:07 +0300

    Easier way to create archives.

--- theory/working_with_files/9_archiving.py
+++ theory/working_with_files/9_archiving.py
@@ -256,3 +256,32 @@ shutil.rmtree('tar_archive')
 # The 'w:gz' mode opens the archive for gzip compressed writing and 'r:gz' opens the archive for gzip compressed
 # reading. Opening compressed archives in append mode is not possible. To add files to a compressed archive, you have
 # to create a new archive.
+
+
+# AN EASIER WAY TO CREATING ARCHIVES
+# https://realpython.com/working-with-files-in-python/#an-easier-way-of-creating-archives
+
+# The Python Standard Library also supports creating TAR and ZIP archives using the high-level methods in the shutil
+# module. The archiving utilities in shutil allow you to create, read, and extract ZIP and TAR archives. These utilities
+# rely on the lower level tarfile and zipfile modules.
+
+# shutil.make_archive() takes at least two arguments: the name of the archive and an archive format.
+
+# By default, it compresses all the files in the current directory into the archive format specified in the format
+# argument. You can pass in an optional root_dir argument to compress files in a different directory.
+# .make_archive() supports the zip, tar, bztar, and gztar archive formats.
+
+# shutil.make_archive(base_name, format, root_dir)
+shutil.make_archive('archive/backup', 'zip', '.')
+
+# This copies everything in archive/ and creates an archive called backup.zip in the filesystem and returns its name.
+# To extract the archive, call .unpack_archive():
+
+shutil.unpack_archive('archive/backup.zip', 'extract_dir/')
+
+# Calling .unpack_archive() and passing in an archive name and destination directory extracts the contents of backup.tar
+# into extract_dir/. TAR archives can be created and extracted in the same way.
+
+# Delete directories created in this example:
+shutil.rmtree('archive')
+shutil.rmtree('extract_dir')
