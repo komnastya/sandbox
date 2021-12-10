bdf5b37edac8e199f0508e59e866d0c3a5c88f41
Nastassia Kamiakova <nastassia294@gmail.com>
2021-12-10 14:37:00 +0300

    Add tests and improve the normalize function.

--- theory/working_with_files/normalize_path.py
+++ theory/working_with_files/normalize_path.py
@@ -3,10 +3,13 @@ def normalize(path: str) -> str:
     new_parts = []
     for part in path.split('/'):
         if part == '..':
-            if new_parts != []:
+            if new_parts:
                 new_parts.pop()
         elif part == '' or part == '.':
             continue
         else:
             new_parts.append(part)
-    return '/' + '/'.join(new_parts)
+    new_path = '/'.join(new_parts)
+    if path.startswith('/'):
+        return '/' + new_path
+    return new_path
