7f4e517511602e9dba3e8dae438f6f44c1f80741
Nastassia Kamiakova <nastassia294@gmail.com>
2021-12-21 12:19:53 +0300

    Clean up the code

--- project_stats/new_count.py
+++ project_stats/new_count.py
@@ -1,53 +1,56 @@
+from __future__ import annotations
+
 import glob
-import pathlib
 from pathlib import Path
 from typing import Callable, Dict, List, Tuple
 
 
-# Stats just for a single file, for example a PY file.
-class FileStats:
-    def __init__(self, total_line_count, code_line_count, comment_line_count, empty_line_count):
+class CodeStats:
+    def __init__(self, total_line_count: int, code_line_count: int, comment_line_count: int, empty_line_count: int):
         self.total_line_count = total_line_count
         self.code_line_count = code_line_count
         self.comment_line_count = comment_line_count
         self.empty_line_count = empty_line_count
 
     @classmethod
-    def sum(entries: List[FileEntry]) -> FileEntry:
+    def sum(cls, entries: List[FileCodeStats]) -> FileCodeStats:
         pass
+
+    def __str__(self):
+        return f"{self.total_line_count}"
 
 
 # Type, which combines file name with its stats.
-FileEntry = Tuple[
+FileCodeStats = Tuple[
     str,  # File name.
-    FileStats  # File stats.
+    CodeStats  # Code stats.
 ]
 
 
-def py_stats(path: Path, comment: str) -> FileStats:
+def py_stats(path: Path) -> CodeStats:
     with open(path, 'r', encoding="utf-8") as f:
-        total_lines = 0
+        lines = 0
         empty_lines = 0
         code_lines = 0
         comment_lines = 0
         for line in f.readlines():
-            total_lines += 1
+            lines += 1
             line = line.strip()
             if line == '':
                 empty_lines += 1
-            elif line.startswith(comment):
+            elif line.startswith('#'):
                 comment_lines += 1
             else:
                 code_lines += 1
-    return FileStats(total_lines, code_lines, comment_lines, empty_lines)
+    return CodeStats(lines, code_lines, comment_lines, empty_lines)
 
 
-def txt_stats(path: Path) -> FileStats:
+def txt_stats(path: Path) -> CodeStats:
     lines = 0
     with open(path, 'r', encoding="utf-8") as f:
         for _ in f.readlines():
             lines += 1
-    return FileStats(lines, 0, 0, 0)
+    return CodeStats(lines, 0, 0, 0)
 
 
 FILE_TYPES = {
@@ -59,36 +62,37 @@ FILE_TYPES = {
 }
 
 
-def scan_files(path: Path) -> List[FileEntry]:
-    # For each found file...
-    all_files = []
-    for entry in glob.glob(f'{path}/**/*', recursive=True):
-        stats = FILE_TYPES.get(entry.suffix)
+def scan_files(dir: Path) -> List[FileCodeStats]:
+    result = []
+    for entry in glob.glob(f'{dir}/**/*', recursive=True):
+        path = Path(entry)
+        stats = FILE_TYPES.get(path.suffix)
         if stats is not None:
-            all_files.append((path.name, stats(path)))
-    return all_files
+            result.append((str(path), stats(path)))
+    return result
 
 
-def by_suffix(entry: FileEntry) -> str:
+def by_suffix(entry: FileCodeStats) -> str:
     return Path(entry[0]).suffix
 
 
-def by_test_not_test(entry: FileEntry) -> str:
+def by_main_or_test(entry: FileCodeStats) -> str:
     path = Path(entry[0])
     if path.name.endswith('_test.py') or path.name.startswith('test_'):
         return 'test'
     else:
-        return 'not_test'
+        return 'main'
 
 
 # Groups files and gathers stats about file group
-def group_by(entries: List[FileEntry], key_of: Callable[[FileEntry], str]) -> Dict[str, List[FileEntry]]:
+def group_by(entries: List[FileCodeStats], key_of: Callable[[FileCodeStats], str]) -> Dict[str, List[FileCodeStats]]:
     pass
 
 
-all_files = scan_files(pathlib.Path(__file__).parent.parent)  # all files in sandbox project
+all_files = scan_files(Path(__file__).parent.parent)  # all files in sandbox project
 by_suffix_dict = group_by(all_files, by_suffix)  # files grouped by file type
 py_files = by_suffix_dict['.py']  # python files
-py_files_by_test_not_test = group_by(py_files, by_test_not_test)  # python files by test/code
-py_code_files = py_files_by_test_not_test['not_test']  # python code files
-py_test_files = py_files_by_test_not_test['test']  # python test files
+py_files_by_main_or_test = group_by(py_files, by_main_or_test)  # python files by test/code
+py_main_files = py_files_by_main_or_test['main']  # python main files
+py_test_files = py_files_by_main_or_test['test']  # python test files
+print(CodeStats.sum(py_main_files))
