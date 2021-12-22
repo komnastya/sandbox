beca4eaee52b414e9b735d4c075b67a782d8b52c
Nastassia Kamiakova <nastassia294@gmail.com>
2021-12-22 21:36:43 +0300

    Implement print_rows function

--- project_stats/new_represent.py
+++ project_stats/new_represent.py
@@ -1,9 +1,12 @@
-from new_count import CodeStats, by_main_or_test, by_suffix, group_by, scan_files
+import pathlib
+from typing import Dict, List
+
+from new_count import CodeStats, FileCodeStats, by_main_or_test, by_suffix, group_by, scan_files
 
 
-def represent(dir_to_scan):
+def represent(dir_to_scan: pathlib.Path) -> None:
     all_files = scan_files(dir_to_scan)
-    files_group = group_by(all_files, by_suffix)
+    files_groups = group_by(all_files, by_suffix)
     # >>> Dict[.py: List[FileCodeStats], .txt: List[FileCodeStats], ... ]
 
     w = 12
@@ -13,8 +16,35 @@ def represent(dir_to_scan):
         '.txt': 'Text',
         '.md': 'Markdown',
         '.css': 'CSS',
-        '.html': 'HTML'
+        '.html': 'HTML',
+        'test': 'Test files',
+        'main': 'Main files'
     }
+
+    def _print_rows(files_groups: Dict[str, List[FileCodeStats]], files_lists) -> None:
+        groups_stats = CodeStats.sum([file[1] for file in files_lists])
+
+        for group_name, files_list in files_groups.items():
+
+            group_stats = CodeStats.sum([file[1] for file in files_list])
+
+            name = names.get(group_name, group_name)
+
+            if group_stats.total_line_count > 0 and group_stats.code_line_count == \
+                    group_stats.comment_line_count == group_stats.empty_line_count == 0:
+                print(f"{name:<{w}}{len(files_list):^{w}}{len(files_list) / len(files_lists): ^ {w}.2%}")
+            else:
+                print(f"{name:<{w}}{len(files_list):^{w}}{len(files_list) / len(files_lists):^{w}.2%}"
+                      f"{group_stats.code_line_count:^{w}}{group_stats.code_percentage:^{w}.2%}"
+                      f"{group_stats.comment_line_count:^{w}}{group_stats.comment_percentage:^{w}.2%}"
+                      f"{group_stats.empty_line_count:^{w}}{group_stats.empty_percentage:^{w}.2%}"
+                      f"{group_stats.total_line_count:^{w}}")
+
+        print(f"\n{'Total':<{w}}{len(files_lists):^{w}}{1:^{w}.0%}"
+              f"{groups_stats.code_line_count:^{w}}{'':^{w}}"
+              f"{groups_stats.comment_line_count:^{w}}{'':^{w}}"
+              f"{groups_stats.empty_line_count:^{w}}{'':^{w}}"
+              f"{groups_stats.total_line_count:^{w}}")
 
     def dir_structure():
 
@@ -31,20 +61,7 @@ def represent(dir_to_scan):
                   f"\n{'-' * w * 10}")
 
             # print directory structure
-            for group_name, files_list in files_group.items():
-                group_stats = CodeStats.sum([file[1] for file in files_list])
-
-                name = names.get(group_name, group_name)
-
-                if group_stats.total_line_count > 0 and group_stats.code_line_count == \
-                        group_stats.comment_line_count == group_stats.empty_line_count == 0:
-                    print(f"{name:<{w}}{len(files_list):^{w}}{len(files_list) / len(all_files):^{w}.1%}")
-                else:
-                    print(f"{name:<{w}}{len(files_list):^{w}}{len(files_list) / len(all_files):^{w}.1%}"
-                          f"{group_stats.code_line_count:^{w}}{group_stats.code_percentage:^{w}.1%}"
-                          f"{group_stats.comment_line_count:^{w}}{group_stats.comment_percentage:^{w}.1%}"
-                          f"{group_stats.empty_line_count:^{w}}{group_stats.empty_percentage:^{w}.1%}"
-                          f"{group_stats.total_line_count:^{w}}")
+            _print_rows(files_groups, all_files)
 
         else:
             # print statement which says that directory is empty
@@ -53,7 +70,7 @@ def represent(dir_to_scan):
                   f"\n{'-' * w * 10}\n")
 
     def python_files_structure():
-        python_files = files_group.get('.py')  # List[FileCodeStats]
+        python_files = files_groups.get('.py')  # List[FileCodeStats]
 
         if python_files:
             # >>> List[(file_name, CodeStats)]]
@@ -68,49 +85,7 @@ def represent(dir_to_scan):
                   f"\n{'-' * w * 10}")
 
             python_files_group = group_by(python_files, by_main_or_test)
-            # >>> Dict[main: List[(file_name, CodeStats)], test: List[(file_name, CodeStats)]]
-
-            main_python_files = python_files_group.get('main')
-            # List[(file_name, CodeStats)] -> main python files
-
-            test_python_files = python_files_group.get('test')
-            # List[(file_name, CodeStats)] -> test python files
-
-            if main_python_files:
-                main_stats = CodeStats.sum([main[1] for main in main_python_files])
-
-                print(f"{'Main files':<{w}}{len(main_python_files):^{w}}"
-                      f"{len(main_python_files) / len(python_files) :^{w}.1%}"
-                      f"{main_stats.code_line_count:^{w}}"
-                      f"{main_stats.code_line_count / python_stats.code_line_count:^{w}.1%}"
-                      f"{main_stats.comment_line_count:^{w}}"
-                      f"{main_stats.comment_line_count / python_stats.comment_line_count:^{w}.1%}"
-                      f"{main_stats.empty_line_count:^{w}}"
-                      f"{main_stats.empty_line_count / python_stats.empty_line_count:^{w}.1%}"
-                      f"{main_stats.total_line_count:^{w}}")
-
-            if test_python_files:
-                test_stats = CodeStats.sum([test[1] for test in test_python_files])
-
-                print(f"{'Test files':<{w}}{len(test_python_files):^{w}}"
-                      f"{len(test_python_files) / len(python_files) :^{w}.1%}"
-                      f"{test_stats.code_line_count:^{w}}"
-                      f"{test_stats.code_line_count / python_stats.code_line_count:^{w}.1%}"
-                      f"{test_stats.comment_line_count:^{w}}"
-                      f"{test_stats.comment_line_count / python_stats.comment_line_count:^{w}.1%}"
-                      f"{test_stats.empty_line_count:^{w}}"
-                      f"{test_stats.empty_line_count / python_stats.empty_line_count:^{w}.1%}"
-                      f"{test_stats.total_line_count:^{w}}")
-
-            print(
-                f"{'All files':<{w}}{len(python_files):^{w}}{len(python_files) / len(python_files) :^{w}.1%}"
-                f"{python_stats.code_line_count:^{w}}"
-                f"{python_stats.code_line_count / python_stats.code_line_count:^{w}.1%}"
-                f"{python_stats.comment_line_count:^{w}}"
-                f"{python_stats.comment_line_count / python_stats.comment_line_count:^{w}.1%}"
-                f"{python_stats.empty_line_count:^{w}}"
-                f"{python_stats.empty_line_count / python_stats.empty_line_count:^{w}.1%}"
-                f"{python_stats.total_line_count:^{w}}\n")
+            _print_rows(python_files_group, python_files)
 
     dir_structure()
     python_files_structure()
