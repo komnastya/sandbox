d6fa24ac6fc7e168bde0b3a8e6b9a1eaa19e8519
Nastassia Kamiakova <nastassia294@gmail.com>
2021-12-22 13:55:57 +0300

    Implement print_python_files_structure

--- project_stats/new_represent.py
+++ project_stats/new_represent.py
@@ -1,6 +1,6 @@
 import pathlib
 
-from new_count import CodeStats, by_suffix, group_by, scan_files
+from new_count import CodeStats, by_main_or_test, by_suffix, group_by, scan_files
 
 
 def represent_new(dir_to_scan):
@@ -16,36 +16,92 @@ def represent_new(dir_to_scan):
 
     python_stats = CodeStats.sum([python_file[1] for python_file in python_files])
 
-    w = 15
+    w = 12
 
     def print_dir_name():
         header = f'structure of {dir_to_scan.name.upper()} directory'
-        print(f"\n{header:^{w * 7}}"
-              f"\n{'-' * w * 7}")
+        print(f"\n{header:^{w * 10}}"
+              f"\n{'-' * w * 10}")
 
     def print_header():
-        print(f"{'Language':^{w}}{'Quantity':^{w}}{'%':^{w}}{'Code lines':^{w}}"
-              f"{'Comment lines':^{w}}{'Empty lines':^{w}}{'Total lines':^{w}}")
+        print(f"{'Language':^{w}}{'Quantity':^{w}}{'%':^{w}}{'Code lines':^{w}}{'%':^{w}}"
+              f"{'Comment lines':^{w}}{'%':^{w}}{'Empty lines':^{w}}{'%':^{w}}{'Total lines':^{w}}")
 
     def print_general_dir_structure():
         if python_files:
             print(f"{'Python':<{w}}{len(python_files):^{w}}{len(python_files) / len(all_files) :^{w}.1%}"
-                  f"{python_stats.code_line_count:^{w}}{python_stats.comment_line_count:^{w}}"
-                  f"{python_stats.empty_line_count:^{w}}{python_stats.total_line_count:^{w}}")
+                  f"{python_stats.code_line_count:^{w}}{python_stats.code_percentage:^{w}.1%}"
+                  f"{python_stats.comment_line_count:^{w}}{python_stats.comment_percentage:^{w}.1%}"
+                  f"{python_stats.empty_line_count:^{w}}{python_stats.empty_percentage:^{w}.1%}"
+                  f"{python_stats.total_line_count:^{w}}")
         if html_files:
-            print(f"{'HTML':<{w}}{len(html_files):^{w}}{'':^{w}.1%}")
+            print(f"{'HTML':<{w}}{len(html_files):^{w}}{len(html_files) / len(all_files) :^{w}.1%}")
         if css_files:
             print(f"{'CSS':<{w}}{len(css_files):^{w}}{len(css_files) / len(all_files) :^{w}.1%}")
         if txt_files:
-            print(f"{'Text files':<{w}}{len(txt_files):^{w}}{len(txt_files) / len(all_files) :^{w}.1%}")
+            print(f"{'Text':<{w}}{len(txt_files):^{w}}{len(txt_files) / len(all_files) :^{w}.1%}")
         if md_files:
-            print(f"{'Markdown files':<{w}}{len(md_files):^{w}}{len(md_files) / len(all_files) :^{w}.1%}")
+            print(f"{'Markdown':<{w}}{len(md_files):^{w}}{len(md_files) / len(all_files) :^{w}.1%}")
         if all_files:
             print(f"{'Total':<{w}}{len(all_files):^{w}}{len(all_files) / len(all_files) :^{w}.1%}")
+
+    def print_python_files_structure():
+        # >>> List[(file_name, CodeStats)]]
+
+        print(f"\n{'PYTHON FILES STRUCTURE':^{w * 10}}")
+        print('-' * w * 10)
+
+        print(f"{'File group':^{w}}{'Quantity':^{w}}{'%':^{w}}{'Code lines':^{w}}{'%':^{w}}"
+              f"{'Comment lines':^{w}}{'%':^{w}}{'Empty lines':^{w}}{'%':^{w}}{'Total lines':^{w}}")
+
+        python_files_group = group_by(python_files, by_main_or_test)
+        # >>> Dict[main: List[(file_name, CodeStats)], test: List[(file_name, CodeStats)]]
+
+        main_python_files = python_files_group.get('main')
+        # List[(file_name, CodeStats)] -> main python files
+
+        test_python_files = python_files_group.get('test')
+        # List[(file_name, CodeStats)] -> test python files
+
+        if main_python_files:
+            main_stats = CodeStats.sum([main[1] for main in main_python_files])
+
+            print(f"{'Main files':<{w}}{len(main_python_files):^{w}}"
+                  f"{len(main_python_files) / len(python_files) :^{w}.1%}"
+                  f"{main_stats.code_line_count:^{w}}"
+                  f"{main_stats.code_line_count / python_stats.code_line_count:^{w}.1%}"
+                  f"{main_stats.comment_line_count:^{w}}"
+                  f"{main_stats.comment_line_count / python_stats.comment_line_count:^{w}.1%}"
+                  f"{main_stats.empty_line_count:^{w}}"
+                  f"{main_stats.empty_line_count / python_stats.empty_line_count:^{w}.1%}"
+                  f"{main_stats.total_line_count:^{w}}")
+
+        if test_python_files:
+            test_stats = CodeStats.sum([test[1] for test in test_python_files])
+            print(f"{'Test files':<{w}}{len(test_python_files):^{w}}"
+                  f"{len(test_python_files) / len(python_files) :^{w}.1%}"
+                  f"{test_stats.code_line_count:^{w}}"
+                  f"{test_stats.code_line_count / python_stats.code_line_count:^{w}.1%}"
+                  f"{test_stats.comment_line_count:^{w}}"
+                  f"{test_stats.comment_line_count / python_stats.comment_line_count:^{w}.1%}"
+                  f"{test_stats.empty_line_count:^{w}}"
+                  f"{test_stats.empty_line_count / python_stats.empty_line_count:^{w}.1%}"
+                  f"{test_stats.total_line_count:^{w}}")
+
+        print(
+            f"{'All files':<{w}}{len(python_files):^{w}}{len(python_files) / len(python_files) :^{w}.1%}"
+            f"{python_stats.code_line_count:^{w}}"
+            f"{python_stats.code_line_count / python_stats.code_line_count:^{w}.1%}"
+            f"{python_stats.comment_line_count:^{w}}"
+            f"{python_stats.comment_line_count / python_stats.comment_line_count:^{w}.1%}"
+            f"{python_stats.empty_line_count:^{w}}"
+            f"{python_stats.empty_line_count / python_stats.empty_line_count:^{w}.1%}"
+            f"{python_stats.total_line_count:^{w}}\n")
 
     print_dir_name()
     print_header()
     print_general_dir_structure()
+    print_python_files_structure()
 
 
 represent_new(pathlib.Path(__file__).parent.parent)
